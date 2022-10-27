391cc1c4d2777f66dee6a2a2292adb36688d8836
AxelTEC <al20760710@ite.edu.mx>
2022-10-26 18:41:59 -0700

    Funcionalidad borrar tarea y modularicion de FormTask

--- src/components/tareas/FormTask.jsx
+++ src/components/tareas/FormTask.jsx
@@ -1,21 +1,32 @@
 import { useState, useEffect } from "react";
-import { Icon } from "@iconify/react";
+import {
+	ErrorFormTask,
+	InputFormTask,
+	LayoutFormTask,
+	SelectFormTask,
+	SubmitFormTask,
+	DeleteFormTask,
+	TitleFormTask,
+} from "./FormTask_Components";
 
 const FormTask = ({
-	setShowFormTask,
+	handleCloseForm,
 	handleEditTask,
 	handleCreateTask,
 	editTask,
+	handleDeleteTask,
 }) => {
 	const [isEditing, setIsEditing] = useState(false);
 	const [error, setError] = useState(false);
 	const [form, setForm] = useState({ description: "", sector: "AB" });
+
 	useEffect(() => {
 		if (Object.keys(editTask).length !== 0) {
 			setIsEditing(true);
 			setForm(editTask);
 		}
 	}, []);
+
 	const handleChange = (e) => {
 		setForm({
 			...form,
@@ -23,6 +34,7 @@ const FormTask = ({
 		});
 		form.description !== "" && error && setError(false);
 	};
+
 	const handleSubmit = (e) => {
 		e.preventDefault();
 		if (!form.description || !form.sector) {
@@ -32,62 +44,26 @@ const FormTask = ({
 		if (isEditing) handleEditTask(form);
 		else handleCreateTask(form);
 	};
-	return (
-		<div
-			onClick={() => setShowFormTask(false)}
-			className="w-full h-screen absolute left-0 backdrop-blur-sm top-0 z-1 flex justify-center items-center cursor-pointer z-20"
-		>
-			<div
-				onClick={(e) => e.stopPropagation()}
-				className="bg-white p-4 rounded-md w-4/6 shadow-lg shadow-black/30 max-w-sm cursor-default fixed overflow-hidden"
-			>
-				{error && (
-					<div className="bg-red-500 w-full  absolute top-0 left-0">
-						<span className="text-sm text-white text-center block">
-							Debes llenar todos los campos
-						</span>
-					</div>
-				)}
-				<h1 className="text-blue-500 font-bold text-xl text-center mt-1">
-					{isEditing ? "EDITAR" : "AGREGAR"} TAREA
-				</h1>
-				<h2 className="text-center text-cyan-700 text-sm font-semibold">
-					No: 001
-				</h2>
-				<form onSubmit={handleSubmit}>
-					<label className="block mt-2">Descripción:</label>
-					<input
-						className={
-							"block border-2  rounded-md w-full p-1 mt-2" +
-							(error ? " border-red-500 " : " border-blue-500 ")
-						}
-						type="text"
-						placeholder="Describa aquí su tarea"
-						name="description"
-						onChange={handleChange}
-						value={form.description}
-					/>
-					<div className="flex justify-between items-center">
-						<div className="w-4/6">
-							<label className="block mt-2">Sector:</label>
-							<select
-								className="block border-2 border-blue-500 rounded-md w-full py-1 mt-2"
-								name="sector"
-								onChange={handleChange}
-								value={form.sector}
-							>
-								<option value="AB">AB</option>
-								<option value="BC">BC</option>
-							</select>
-						</div>
 
-						<button className="flex justify-center rounded-md items-center w-12 self-end h-12 bg-[#019054] shadow-md shadow-black/30">
-							<Icon icon="fluent-mdl2:accept-medium" color="white" width={30} />
-						</button>
-					</div>
-				</form>
-			</div>
-		</div>
+	return (
+		<LayoutFormTask handleCloseForm={handleCloseForm}>
+			{error && <ErrorFormTask />}
+			<TitleFormTask title={(isEditing ? "EDITAR" : "AGREGAR") + " TAREA"} />
+			<form onSubmit={handleSubmit}>
+				<InputFormTask
+					border={error ? " border-red-500 " : " border-blue-500 "}
+					handleChange={handleChange}
+					value={form.description}
+				/>
+				<div className="flex justify-between items-center">
+					<SelectFormTask handleChange={handleChange} value={form.sector} />
+					<SubmitFormTask
+						bg={!isEditing ? " bg-[#019054] " : " bg-yellow-500 "}
+					/>
+				</div>
+			</form>
+			{isEditing && <DeleteFormTask handleDeleteTask={handleDeleteTask} />}
+		</LayoutFormTask>
 	);
 };
 
