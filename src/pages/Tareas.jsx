a5897c32a234c4395a1f68e16b20bb8c40b69d56
AxelTEC <al20760710@ite.edu.mx>
2022-10-26 17:04:07 -0700

    Pagina tareas funcionando con estado

--- src/pages/Tareas.jsx
+++ src/pages/Tareas.jsx
@@ -1,68 +1,92 @@
 import { useState } from "react";
 import NavBar from "../components/global/NavBar";
-import Tr from "../components/tareas/Tr";
-import Th from "../components/tareas/Th";
 import BtnAddTask from "../components/tareas/BtnAddTask";
+import FormTask from "../components/tareas/FormTask";
+import Layout from "../components/tareas/Layout";
+import Table from "../components/tareas/Table";
 
 const Tareas = () => {
+	const [showFormTask, setShowFormTask] = useState(false);
+	const [editTask, setEditTask] = useState({});
 	const [testData, setTestData] = useState([
 		{
 			id: 1,
-			name: "Irrigar surcos.",
+			description: "Irrigar surcos.",
 			sector: "AB",
 			status: "Pendiente",
 		},
 		{
 			id: 2,
-			name: "Podar los plantios",
+			description: "Podar los plantios",
 			sector: "AB",
 			status: "Progreso",
 		},
 		{
 			id: 3,
-			name: "Limpiar surcos",
-			sector: "AB",
+			description: "Limpiar surcos",
+			sector: "RD",
 			status: "Terminada",
 		},
 		{
 			id: 4,
-			name: "Piscar los olivos",
+			description: "Piscar los olivos",
 			sector: "AB",
 			status: "Pendiente",
 		},
 		{
 			id: 5,
-			name: "Conteo de uvas ",
-			sector: "AB",
+			description: "Conteo de uvas ",
+			sector: "BC",
 			status: "Terminada",
 		},
 	]);
+	const handleEditTask = (newTask) => {
+		const newTestData = testData.map((task) =>
+			task.id === editTask.id ? newTask : task
+		);
+		setTestData(newTestData);
+		setEditTask({});
+		setShowFormTask(false);
+	};
+
+	const handleCreateTask = (newTask) => {
+		newTask.id = testData.length + 1;
+		newTask.status = "Pendiente";
+		setTestData([...testData, newTask]);
+		setShowFormTask(false);
+	};
+
+	const sortTable = (column, isAsc) => {
+		let auxArray = [...testData];
+		auxArray.sort((a, b) => {
+			if (a[column] > b[column]) return 1;
+			if (a[column] < b[column]) return -1;
+			return 0;
+		});
+		!isAsc && auxArray.reverse();
+		setTestData(auxArray);
+	};
+
 	return (
 		<>
 			<NavBar title={"Tareas"} />
-			<div className="max-w-2xl m-auto ">
-				<table className="w-full border-spacing-2 md:mt-10 md:shadow-md md:shadow-zinc-700/50">
-					<thead className="bg-[#0089BA]/30 h-10">
-						<tr className="text-black/60 space-x-1">
-							<Th size="w-6/12" title="Nombre" />
-							<Th size="w-2/12" title="Sector" center />
-							<Th size="w-4/12" title="Estado" center />
-						</tr>
-					</thead>
-					<tbody>
-						{testData &&
-							testData.map((task) => (
-								<Tr
-									key={task.id}
-									name={task.name}
-									sector={task.sector}
-									status={task.status}
-								/>
-							))}
-					</tbody>
-				</table>
-				<BtnAddTask />
-			</div>
+			<Layout>
+				<Table
+					tasks={testData}
+					setEditTask={setEditTask}
+					setShowFormTask={setShowFormTask}
+					sortTable={sortTable}
+				/>
+				{showFormTask && (
+					<FormTask
+						setShowFormTask={setShowFormTask}
+						handleEditTask={handleEditTask}
+						handleCreateTask={handleCreateTask}
+						editTask={editTask}
+					/>
+				)}
+				<BtnAddTask setShowFormTask={setShowFormTask} />
+			</Layout>
 		</>
 	);
 };
