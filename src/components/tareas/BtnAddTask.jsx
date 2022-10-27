35c8ddc0772e5e59ca963bcfed099ea3fb403733
AxelTEC <al20760710@ite.edu.mx>
2022-10-26 21:23:53 -0700

    Ajuste de responsividad

--- src/components/tareas/BtnAddTask.jsx
+++ src/components/tareas/BtnAddTask.jsx
@@ -5,7 +5,7 @@ const BtnAddTask = ({ setShowFormTask }) => {
 	return (
 		<button
 			onClick={() => setShowFormTask(true)}
-			className="absolute w-14 h-14 shadow-md shadow-black/30 rounded-lg bg-[#FF6C3E] text-2xl font-bold text-white right-5 bottom-5 grid place-content-center"
+			className="fixed w-14 h-14 shadow-md shadow-black/30 rounded-lg bg-[#FF6C3E] text-2xl font-bold text-white right-5 bottom-5 grid place-content-center"
 		>
 			<Icon icon="ic:twotone-add" width={40} />
 		</button>
