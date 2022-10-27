565106026ec390cb364c3fd3c500a14d6b5f2469
AxelTEC <al20760710@ite.edu.mx>
2022-10-26 23:57:13 -0700

    Empleado prueba

--- src/pages/Empleados.jsx
+++ src/pages/Empleados.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import BtnAdd from "../components/global/BtnAdd";
+import NavBar from "../components/global/NavBar";
+import Table from "../components/global/table/Table";
+import Th from "../components/global/table/Th";
+import THead from "../components/global/table/THead";
+import Tr from "../components/global/table/Tr";
 
 const Empleados = () => {
-	return <div>Empleados</div>;
+	return (
+		<>
+			<NavBar title="Empleados" />
+			<div className="max-w-2xl m-auto">
+				<Table>
+					<THead>
+						<Th title="Nombre" size="w-10/12" />
+						<Th title="Sector" size="w-2/12" center />
+					</THead>
+					<tbody>
+						<Tr tableData={["Axel Adrian Enciso Robles", "Ab"]} />
+						<Tr tableData={["Axel Adrian Enciso Robles", "Ab"]} />
+						<Tr tableData={["Axel Adrian Enciso Robles", "Ab"]} />
+						<Tr tableData={["Axel Adrian Enciso Robles", "Ab"]} />
+						<Tr tableData={["Axel Adrian Enciso Robles", "Ab"]} />
+						<Tr tableData={["Axel Adrian Enciso Robles", "Ab"]} />
+					</tbody>
+				</Table>
+			</div>
+			<BtnAdd />
+		</>
+	);
 };
 
 export default Empleados;
