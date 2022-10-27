dbc3bd878da694f434e53c75b25e9721384f5779
AxelTEC <al20760710@ite.edu.mx>
2022-10-26 17:18:08 -0700

    Ajuste responsividad

---
+++ src/components/tareas/BtnAddTask.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Icon } from "@iconify/react";
+
+const BtnAddTask = ({ setShowFormTask }) => {
+	return (
+		<button
+			onClick={() => setShowFormTask(true)}
+			className="absolute w-14 h-14 shadow-md shadow-black/30 rounded-lg bg-[#FF6C3E] text-2xl font-bold text-white right-5 bottom-5 grid place-content-center"
+		>
+			<Icon icon="ic:twotone-add" width={40} />
+		</button>
+	);
+};
+
+export default BtnAddTask;
