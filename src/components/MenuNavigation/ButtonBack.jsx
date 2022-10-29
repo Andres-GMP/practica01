190e704ff1b4c8c826c956319f9d314e99b9f5b3
Andres-GMP <al20760260@ite.edu.mx>
2022-10-29 00:07:12 -0700

    no style, but works

--- src/components/MenuNavigation/ButtonBack.jsx
+++ src/components/MenuNavigation/ButtonBack.jsx
@@ -3,16 +3,14 @@ import { Link } from "react-router-dom";
 
 // Button to navigate
 const ButtonBack = ({ text, pathSource, pathImg }) => {
-    return (
-        <>
-            <Link path={pathSource} className="w-full h-40 flex content-center ">
-                {pathImg}
-                <span>
-                    {text}
-                </span>
-            </Link>
-        </>
-    );
+  return (
+    <>
+      <Link to={pathSource} className="w-full h-40 flex content-center ">
+        {pathImg}
+        <span>{text}</span>
+      </Link>
+    </>
+  );
 };
 
-export default Layout;
+export default ButtonBack;
