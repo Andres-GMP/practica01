fdaeb95dbec789fc3096cf7b6a6eb4d6a7209390
Andres-GMP <al20760260@ite.edu.mx>
2022-10-28 23:16:33 -0700

    Button Navigation with props, none modules

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import Layout from "../components/MenuNavigation/Layout";
+import ButtonBack from "../components/MenuNavigation/ButtonBack"
+import { Icon } from '@iconify/react';
 
 const Home = () => {
-	return <div>Home</div>;
+	return <>
+		<Layout>
+			<ButtonBack pathImg={
+				<Icon icon="ant-design:home-filled" />
+			} text="Inicio" pathSource="/admin">
+			</ButtonBack>
+			
+			<ButtonBack pathImg={
+				<Icon icon="bi:pin-angle-fill" />
+			} text="Tareas" pathSource="/admin/tareas">
+			</ButtonBack>
+
+			<ButtonBack pathImg={
+				<Icon icon="bxs:user" />
+			} text="Empleado" pathSource="/admin/empleados">
+			</ButtonBack>
+		</Layout>
+	</>
+
 };
 
 export default Home;
